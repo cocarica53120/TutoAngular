49d18ae9dc8bd22adbbc5d25fbdf8240c90167ab
Alain Collet <Alain.Collet@harmonicinc.com>
2020-11-06 16:49:47 +0100

    Added mock-pokemons and implementation of OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
 	selector: 'pokemon-app',
-	templateUrl: './app/app.component.html'
+	// templateUrl: './app/app.component.html'
+	template: `<h1> Liste de Pokemons </h1>`
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+
+	private pokemons: Pokemon[];
+
+	ngOnInit() {
+		this.pokemons = POKEMONS;
+		console.log('ngOnInit', this);
+	}
+
+	selectPokemon(pokemon: Pokemon) {
+		alert('Vous avez clické sur ' + pokemon.name);
+	}
+
+	ngOnDestroy() {
+		console.log('ngOnDestroy', this);
+	}
+ }
